cfbd211348d04bddf206f6aaed947ee09a8387d5
SergeyDzyuba <qpopuk@yandex.ru>
2016-07-02 23:24:27 +0300

    Finish static pages

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,20 +16,33 @@
 # users commonly want.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+
+
+# RSpec.configure do |config|
+#   # rspec-expectations config goes here. You can use an alternate
+#   # assertion/expectation library such as wrong or the stdlib/minitest
+#   # assertions if you prefer.
+#   config.expect_with :rspec do |expectations|
+#     # This option will default to `true` in RSpec 4. It makes the `description`
+#     # and `failure_message` of custom matchers include text for helper methods
+#     # defined using `chain`, e.g.:
+#     #     be_bigger_than(2).and_smaller_than(4).description
+#     #     # => "be bigger than 2 and smaller than 4"
+#     # ...rather than:
+#     #     # => "be bigger than 2"
+#     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+#
+#     config.include Capybara::DSL
+#   end
+
 RSpec.configure do |config|
-  # rspec-expectations config goes here. You can use an alternate
-  # assertion/expectation library such as wrong or the stdlib/minitest
-  # assertions if you prefer.
-  config.expect_with :rspec do |expectations|
-    # This option will default to `true` in RSpec 4. It makes the `description`
-    # and `failure_message` of custom matchers include text for helper methods
-    # defined using `chain`, e.g.:
-    #     be_bigger_than(2).and_smaller_than(4).description
-    #     # => "be bigger than 2 and smaller than 4"
-    # ...rather than:
-    #     # => "be bigger than 2"
-    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
-  end
+ config.fixture_path = "#{::Rails.root}/spec/fixtures"
+ config.use_transactional_fixtures = true
+ config.infer_base_class_for_anonymous_controllers = false
+ config.order = "random"
+ config.include Capybara::DSL
+end
+
 
   # rspec-mocks config goes here. You can use an alternate test double
   # library (such as bogus or mocha) by changing the `mock_with` option here.
